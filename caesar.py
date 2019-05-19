0a4d8b809a620401e6a3d3c4d0bb5cb754b60691
eyetpn <eyetpn@gmail.com>
2019-05-19 16:26:47 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,24 +1,26 @@
 from helpers import alphabet_position, rotate_character
-
-def encrypt(text, rot):
 new_text = str(input("Type a message:"))
 rotate = int(input("Please enter rotation:"))
-return_text = []
+return_text = ""
 #enter_text = x.upper
-dictionary = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'
-,'o','p','q','r','s','t','u','v','w','x','y','z']
-dictionary = dictionary + list(map(str.upper, dictionary))
-dictionary2 = ["!", ",", " ", "?", "'", ".", "(", ")", ]
+dictionary = 'abcdefghijklmnopqrstuvwxyz'
+#dictionary = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+#'n','o','p','q','r','s','t','u','v','w','x','y','z']*2
+dictionary2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#dictionary2 =list(map(str.upper, dictionary))
+
+dictionary3 = ["!", ",", "?", "'", ".", "(", ")", ]
 for i in new_text:
-    for j in dictionary:
-        if i == j:
-            new_index = dictionary.index(j) + rotate
+    if i in dictionary:
+        new_index = (dictionary.find(i)+rotate) %26
             #print (new_index)
-            return_text.append (dictionary[new_index])
-    for k in dictionary2:
-        if i == k:
-            return_text.append(i)
-s = ""
-s = s.join(return_text)
-print(s)
-    return
+        return_text += dictionary[new_index]
+    
+    elif i in dictionary2:
+        new_index = (dictionary2.find(i)+rotate) %26
+        return_text += dictionary2[new_index]
+    else:
+        return_text += i
+#s = ""
+#s = s.join(return_text)
+print(return_text)    return
